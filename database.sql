d1301137468b80b998e9f6d5e7d39c3725fec3b6
Alexey Ayzin <alexey.ayzin@gmail.com>
2024-09-25 14:03:30 -0400

    update database creation

--- database.sql
+++ database.sql
@@ -2,21 +2,21 @@ CREATE DATABASE `todo`
 /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */
 /*!80016 DEFAULT ENCRYPTION='N' */
 ;
-CREATE TABLE IF NOT EXISTS `todo`.`user` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(45) NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(100) NOT NULL,
-    `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+CREATE TABLE `todo` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `title` varchar(45) NOT NULL,
+    `description` varchar(255) NOT NULL DEFAULT '',
+    `user_id` int NOT NULL,
+    `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
-    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
-CREATE TABLE IF NOT EXISTS `todo`.`todo` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(45) NOT NULL,
-    `description` VARCHAR(255) NOT NULL DEFAULT '',
-    `user_id` INT NOT NULL,
-    `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 3 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+CREATE TABLE `user` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `name` varchar(45) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `password` varchar(100) NOT NULL,
+    `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
-    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+    UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 6 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
